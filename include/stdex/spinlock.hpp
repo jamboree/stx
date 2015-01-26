05aaa9209bc03a5adcba53f16b42d6fdceddb490
jamboree <tongari95@gmail.com>
2015-01-26 18:07:13 +0800

    Make spinlock match std API

--- include/stdex/spinlock.hpp
+++ include/stdex/spinlock.hpp
@@ -11,22 +11,142 @@
 
 namespace stdex
 {
-	struct spinlock
-	{
-		explicit spinlock(std::atomic_flag& mutex) : _mutex(mutex)
-		{
-			while (!mutex.test_and_set(std::memory_order_acquire));
-		}
+    struct spinlock
+    {
+        spinlock() : _flag{ATOMIC_FLAG_INIT} {}
 
-		~spinlock()
-		{
-			_mutex.clear(std::memory_order_release);
-		}
+        spinlock(spinlock const&) = delete;
 
-	private:
+        void lock()
+        {
+            while (!_flag.test_and_set(std::memory_order_acquire));
+        }
 
-		std::atomic_flag& _mutex;
-	};
+        void unlock()
+        {
+            _flag.clear(std::memory_order_release);
+        }
+
+    private:
+
+        std::atomic_flag _flag;
+    };
+
+    class shared_spinlock
+    {
+        enum ownership : uint32_t { unique = 1, shared = 2 };
+
+    public:
+
+        shared_spinlock() : _flags(0) {}
+
+        shared_spinlock(shared_spinlock const&) = delete;
+
+        void lock()
+        {
+            while (!try_lock());
+        }
+
+        void lock_shared()
+        {
+            while (!try_lock_shared());
+        }
+#if 0
+        template<class Rep, class Period>
+        bool try_lock_for(std::chrono::duration<Rep,Period> const& duration)
+        {
+            auto deadline = std::chrono::steady_clock::now() + duration;
+            while (!try_lock())
+            {
+                if (std::chrono::steady_clock::now() >= deadline)
+                    return false;
+            }
+            return true;
+        }
+
+        template<class Clock, class Duration>
+        bool try_lock_until(std::chrono::time_point<Clock,Duration> const& deadline)
+        {
+            while (!try_lock())
+            {
+                if (Clock::now() >= deadline)
+                    return false;
+            }
+            return true;
+        }
+
+        template<class Rep, class Period>
+        bool try_lock_shared_for(std::chrono::duration<Rep,Period> const& duration)
+        {
+            auto deadline = std::chrono::steady_clock::now() + duration;
+            while (!try_lock_shared())
+            {
+                if (std::chrono::steady_clock::now() >= deadline)
+                    return false;
+            }
+            return true;
+        }
+
+        template<class Clock, class Duration>
+        bool try_lock_shared_until(std::chrono::time_point<Clock,Duration> const& deadline)
+        {
+            while (!try_lock_shared())
+            {
+                if (Clock::now() >= deadline)
+                    return false;
+            }
+            return true;
+        }
+#endif
+        void unlock()
+        {
+            _flags.fetch_and(~unique, std::memory_order_release);
+        }
+
+        void unlock_shared()
+        {
+            _flags.fetch_and(~shared, std::memory_order_release);
+        }
+
+        bool try_lock()
+        {
+            std::uint32_t expect = 0;
+            return _flags.compare_exchange_strong(expect, unique, std::memory_order_acq_rel);
+        }
+
+        bool try_lock_shared()
+        {
+            std::uint32_t value = _flags.fetch_or(shared, std::memory_order_acquire);
+            if (value & unique)
+            {
+                _flags.fetch_and(~shared, std::memory_order_release);
+                return false;
+            }
+            return true;
+        }
+
+    private:
+
+        std::atomic<std::uint32_t> _flags;
+    };
+
+    template<class Mutex>
+    struct shared_lock_guard
+    {
+        explicit shared_lock_guard(Mutex& mutex) : _mutex(mutex)
+        {
+            lock.lock_shared();
+        }
+
+        ~shared_lock_guard()
+        {
+            lock.unlock_shared();
+        }
+
+    private:
+
+        Mutex& _mutex;
+    };
 }
 
 #endif
