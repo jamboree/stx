dab241c9cabe195472af7df644c0e36eab281666
Jamboree <tongari95@gmail.com>
2015-01-26 23:26:56 +0800

    Fix header guard

--- include/stdex/spinlock.hpp
+++ include/stdex/spinlock.hpp
@@ -4,8 +4,8 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //////////////////////////////////////////////////////////////////////////////*/
-#ifndef STDEX_PRIORITY_HPP_INCLUDED
-#define STDEX_PRIORITY_HPP_INCLUDED
+#ifndef STDEX_SPINLOCK_HPP_INCLUDED
+#define STDEX_SPINLOCK_HPP_INCLUDED
 
 #include <atomic>
 
