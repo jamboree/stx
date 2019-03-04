7b8e7d6aab97360b0dfd2ba27d1fd4dbc3be9d76
Jamboree <tongari95@gmail.com>
2019-03-04 23:36:22 +0800

    Update apply_permutation.

--- include/stx/algorithm/apply_permutation.hpp
+++ include/stx/algorithm/apply_permutation.hpp
@@ -1,5 +1,5 @@
 /*//////////////////////////////////////////////////////////////////////////////
-    Copyright (c) 2017 Jamboree
+    Copyright (c) 2017-2019 Jamboree
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -8,28 +8,29 @@
 #define STX_ALGORITHM_APPLY_PERMUTATION_HPP_INCLUDED
 
 #include <utility>
+#include <iterator>
 
 namespace stx
 {
     template<class RandValueIt, class RandIndexIt>
     void apply_permutation(RandValueIt val_it, RandValueIt val_end, RandIndexIt idx_it)
     {
-        auto count = val_end - val_it;
-        using D = decltype(count);
-        D i = 0;
-        for (; i != count; ++i)
+        using D = typename std::iterator_traits<RandIndexIt>::value_type;
+        D count = D(val_end - val_it);
+        for (D i = 0; i != count; ++i)
         {
-            if (idx_it[i] == i)
+            D next = idx_it[i];
+            if (next == i)
                 continue;
             auto tmp(std::move(val_it[i]));
             D curr = i;
             do
             {
-                D next = idx_it[curr];
                 val_it[curr] = std::move(val_it[next]);
                 idx_it[curr] = curr;
                 curr = next;
-            } while (idx_it[curr] != i);
+                next = idx_it[curr];
+            } while (next != i);
             val_it[curr] = std::move(tmp);
             idx_it[curr] = curr;
         }
