f8c268d13c329358c5e3dcc7a0cc7972688a2406
Jamboree <tongari95@gmail.com>
2019-10-05 16:20:05 +0800

    Rename erase_insert -> erase_insert_ordered.

--- include/stx/algorithm/erase_insert_ordered.hpp
+++ include/stx/algorithm/erase_insert_ordered.hpp
@@ -4,15 +4,15 @@
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //////////////////////////////////////////////////////////////////////////////*/
-#ifndef STX_ALGORITHM_ERASE_INSERT_HPP_INCLUDED
-#define STX_ALGORITHM_ERASE_INSERT_HPP_INCLUDED
+#ifndef STX_ALGORITHM_ERASE_INSERT_ORDERED_HPP_INCLUDED
+#define STX_ALGORITHM_ERASE_INSERT_ORDERED_HPP_INCLUDED
 
 #include <algorithm>
 
 namespace stx
 {
     template<class BidIt, class T>
-    BidIt erase_insert(BidIt beg, BidIt end, BidIt it, T val)
+    BidIt erase_insert_ordered(BidIt beg, BidIt end, BidIt it, T val)
     {
         if (it != end)
         {
